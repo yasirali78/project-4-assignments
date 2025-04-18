0e57e8a7699776e8432111b7ec914de91651944c
Yasir Ali <159614142+yasirali78@users.noreply.github.com>
2025-04-18 23:17:43 +0500

    Create assignment_1.py

---
+++ assignment_1.py
@@ -0,0 +1,12 @@
+a = 10
+b = 5
+
+sum_result = a + b
+product = a * b
+difference = a - b
+quotient = a / b
+
+print("Sum:", sum_result)
+print("Product:", product)
+print("Difference:", difference)
+print("Quotient:", quotient)
